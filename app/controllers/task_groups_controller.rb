ffc8df935711a246f775309493fdf4bf1944b8c7
brunomrlima <bruno.mrlima@gmail.com>
2020-08-09 20:33:54 -0400

    jquery working

--- app/controllers/task_groups_controller.rb
+++ app/controllers/task_groups_controller.rb
@@ -3,4 +3,12 @@ class TaskGroupsController < ApplicationController
     @messages = TaskGroup.return_group_information(100)
     @users_array = TaskGroup.return_group_users(100)
   end
+
+  def update_variables
+    @messages = TaskGroup.return_group_information(params[:variables][:n_user].to_i)
+    @users_array = TaskGroup.return_group_users(params[:variables][:n_user].to_i)
+    respond_to do |format|
+      format.js
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root "task_groups#index"
-  resources :task_groups, only: [:index]
+  resources :task_groups, only: [:index] do
+    collection do
+      post :update_variables
+    end
+  end
 end
